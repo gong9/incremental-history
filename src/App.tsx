dd103a4d12e86c40565c5b50d4d4beb3ef1726b6
gong <517441540@qq.com>
2023-05-27 08:48:39 +0800

    fix: adjust example

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,25 @@
-import { useState } from 'react'
-import { Button, Input } from 'antd'
+import type { TabsProps } from 'antd'
+import { Tabs } from 'antd'
 
+import BaseExample from './components/Base'
 import './App.css'
-import historyCcontroller from './historyCcontroller'
-import { generateRandomString } from './utils'
-import bigBaseData from './data.json'
 
+const items: TabsProps['items'] = [
+  {
+    key: 'base',
+    label: 'base example',
+    children: <BaseExample />,
+  },
+  {
+    key: 'other',
+    label: 'other',
+    children: '开发中',
+  },
+
+]
 function App() {
-  const [json, setJson] = useState({})
-
-  const add = () => {
-    historyCcontroller.add(`/${generateRandomString(10)}`, '新加的')
-
-    setJson(historyCcontroller.lastData)
-  }
-
-  const undo = () => {
-    setJson(historyCcontroller.undo())
-  }
-
-  const toggle = () => {
-    setJson(bigBaseData)
-  }
-
   return (
-    <div className="main">
-      <h3 className="m-3">高性能增量JSON历史记录测试 「base」</h3>
-
-      <Input.TextArea disabled value={JSON.stringify(json, null, 2)} />
-
-      <div className="btns flex mt-5 justify-start">
-        <Button onClick={add} className="mr-2">
-          添加
-        </Button>
-        <Button className="mr-2" onClick={undo}>
-          撤销
-        </Button>
-      </div>
-    </div>
+    <Tabs className="main" items={items} />
   )
 }
 
---
+++ src/components/Base/index.tsx
@@ -0,0 +1,39 @@
+import { useState } from 'react'
+import { Button, Card, Input } from 'antd'
+
+import historyCcontroller from '../../historyCcontroller'
+import { generateRandomString } from '../../utils'
+import './index.css'
+
+function BaseExample() {
+  const [json, setJson] = useState({})
+
+  const add = () => {
+    historyCcontroller.add(`/${generateRandomString(15)}`, '新加的')
+
+    setJson(historyCcontroller.lastData)
+  }
+
+  const undo = () => {
+    setJson(historyCcontroller.undo())
+  }
+
+  return (
+    <Card className="base">
+      <h3 className="m-3">高性能增量JSON历史记录测试 「base」</h3>
+
+      <Input.TextArea disabled value={JSON.stringify(json, null, 2)} />
+
+      <div className="btns flex mt-5 justify-start">
+        <Button onClick={add} className="mr-2">
+          添加
+        </Button>
+        <Button className="mr-2" onClick={undo}>
+          撤销
+        </Button>
+      </div>
+    </Card>
+  )
+}
+
+export default BaseExample
